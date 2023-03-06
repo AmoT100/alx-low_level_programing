7cc32936d1fa03a1180818ad738aef005c4254d7
AmoT100 <amohelang72@gmail.com>
2023-03-06 03:07:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <studio.h>
-//**
-//*main - main block
-//*return: 0
-//*
+/**
+ * main - main block
+ * return: 0
+ */
 int main(void)
 {
-	puts('/'programming is like develop a multilingual puzzle');
-	return (0);
+	puts(*\“programming is like develop a multilingual puzzle”);
+        return (0);
 }
 
